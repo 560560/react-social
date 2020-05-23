7fd86b4cfeaa5cb253affcb37b0718c36f3205a1
GerasimenkoAN <560560@bk.ru>
2020-05-23 12:24:03 +0500

    Dialogs begining

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,13 +1,35 @@
 import React from 'react';
 import s from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
 
 const Dialogs = (props) => {
-  return (
-    <div>
-      Dialogs
-    </div>
+    return (
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + " " + s.active}>
+                    Anton
+                </div>
+                <div className={s.dialog}>
+                    Elena
+                </div>
+                <div className={s.dialog}>
+                    Mihail
+                </div>
+                <div className={s.dialog}>
+                    Aleksey
+                </div>
+                <div className={s.dialog}>
 
-  );
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How are you?</div>
+                <div className={s.message}>Yo!</div>
+            </div>
+        </div>
+
+    );
 
 }
 
