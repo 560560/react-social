caed812157f7bc50a19962eb625f622285533046
GerasimenkoAN <560560@bk.ru>
2020-06-03 21:00:14 +0500

    Делаю страницу users часть 2

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,46 +2,44 @@ import React from 'react';
 import s from './Users.module.css';
 
 
-
-
-
-
 const Users = (props) => {
 
-  let makeFollow = () => {
-    props.followTo(3);
-  }
-  let makeUnfollow = (userID) => {
-    props.unfollowFrom(3)
-  }
 
-  let usersElements = props.users.map(u =>
-    <div className={s.usersBlock}>
-      <div className={s.userAva_followButton}>
-        <div className={s.userAva}><img src={u.userAvatar} alt=""/></div>
-        <div className={s.followUnfollowButton}>{(u.isFriend) ? <button onClick={makeUnfollow}>Unfollow</button> : <button onClick={makeFollow}>Follow</button>}</div>
-      </div>
-      <div className={s.userInfo}>
-        <div className={s.userName}>{u.fullName}</div>
-        <div className={s.country}>{u.location.country},</div>
-        <div className={s.status}>{u.status}</div>
-        <div className={s.city}>{u.location.city}</div>
-      </div>
-    </div>)
+    let makeFollow = (event) => {
 
+        props.followTo(Number(event.target.id));
+    }
+    let makeUnfollow = (event) => {
 
-
-
-  return (
-    <div>
-      Users
-
-      {usersElements}
+        props.unfollowFrom(Number(event.target.id))
+    }
+    let usersElements = props.users.map(u => 
+        <div className={s.usersBlock}>
+            <div className={s.userAva_followButton}>
+                <div className={s.userAva}><img src={u.userAvatar} alt=""/></div>
+                <div className={s.followUnfollowButton}>{(u.isFriend) ?
+                    <button onClick={makeUnfollow}  id={u.id}>Unfollow</button> :
+                    <button onClick={makeFollow}  id={u.id}>Follow</button>}</div>
+            </div>
+            <div className={s.userInfo}>
+                <div className={s.userName}>{u.fullName}</div>
+                <div className={s.country}>{u.location.country},</div>
+                <div className={s.status}>{u.status}</div>
+                <div className={s.city}>{u.location.city}</div>
+            </div>
+        </div>)
 
 
-    </div>
+    return (
+        <div>
+            Users
 
-  )
+            {usersElements}
+
+
+        </div>
+
+    )
 
 
 }
