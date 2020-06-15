3527dcdc26e66deeba01cf65eae1ed8ef0ae8be2
GerasimenkoAN <560560@bk.ru>
2020-06-15 09:45:47 +0500

    Доработал ProfileStatus.jsx

--- src/components/Profile/ProfileStatus.jsx
+++ src/components/Profile/ProfileStatus.jsx
@@ -4,17 +4,21 @@ import React from "react";
 class ProfileStatus extends React.Component {
     newStatusElement = React.createRef();
     state = {
-        editMode: false
+        editMode: false,
+        localStatus: this.props.status
     }
 
     enableEditMode = () => {
         this.setState({editMode: true})
     }
     disableEditMode = () => {
-        this.props.updateUserStatus(this.newStatusElement.current.value, this.props.myId)
+        this.props.updateUserStatus(this.state.localStatus)
         this.setState({editMode: false})
     }
 
+    onStatusChange =  () => {
+        this.setState({localStatus: this.newStatusElement.current.value})
+    }
 
     render() {
         return (
@@ -22,7 +26,7 @@ class ProfileStatus extends React.Component {
                 {(!this.props.status && !this.state.editMode )&& <span onClick={this.enableEditMode}>Change status</span>}
                 {!this.state.editMode && <span onDoubleClick={this.enableEditMode}>{this.props.status}</span>}
                 {this.state.editMode &&
-                <input ref={this.newStatusElement} autoFocus={true} onBlur={this.disableEditMode} type="text"></input>}
+                <input ref={this.newStatusElement} value={this.state.localStatus} autoFocus={true} onChange={this.onStatusChange} onBlur={this.disableEditMode} type="text"></input>}
             </div>
         );
     }
