78cee209a9eee2355cf1c23f2ac667af796bf5cd
GerasimenkoAN <560560@bk.ru>
2020-06-18 21:33:04 +0500

    Исправил пару ошибок после последнего комита

--- src/components/Dialogs/SendMessageForm/SendMessageForm.jsx
+++ src/components/Dialogs/SendMessageForm/SendMessageForm.jsx
@@ -1,7 +1,7 @@
 
 import React from "react";
 import s from './SendMessageForm.module.css';
-import {Textarea} from "../../Common/FormsControls/FormsControls";
+
 
 
 
--- src/components/Profile/AddPostForm/AddPostForm.jsx
+++ src/components/Profile/AddPostForm/AddPostForm.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 import s from './AddPostForm.module.css';
 
-import {Textarea} from "../../Common/FormsControls/FormsControls";
+
 
 
 
