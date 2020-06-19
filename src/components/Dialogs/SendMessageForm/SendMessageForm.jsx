08022252ff3d845be36338d3ef7d57d9d78680dd
GerasimenkoAN <560560@bk.ru>
2020-06-19 11:37:07 +0500

    Перенес форму отправки постов в Dialogs на Formik

--- src/components/Dialogs/SendMessageForm/SendMessageForm.jsx
+++ src/components/Dialogs/SendMessageForm/SendMessageForm.jsx
@@ -1,14 +1,41 @@
 
 import React from "react";
 import s from './SendMessageForm.module.css';
+import * as Yup from "yup"
+import {Formik, Form, Field, ErrorMessage} from "formik"
+import FormErrorMessage from "../../Common/FormsControls/FormErrorMessage";
 
+let initValue = {
+  messageText: ""
+}
+
+let validationSchema = Yup.object({
+  messageText: Yup.string().max(10, "Message must not exceed 10 characters")
+  }
+)
 
 
 
 const SendMessageForm = (props) => {
   return (
-   <></>
-  )
-}
 
+    <Formik onSubmit={props.onSubmit} initialValues={initValue} validationSchema={validationSchema}>
+
+      <Form>
+        <div className={s.addMessage}>
+        <Field as="textarea" name="messageText" id="messageText" placeholder="Enter your message"/>
+        <ErrorMessage name="messageText" component={FormErrorMessage}/>
+        </div>
+
+        <div className={s.button}>
+          <button>Add post</button>
+        </div>
+
+      </Form>
+
+
+    </Formik>
+
+
+  )}
 export default SendMessageForm
